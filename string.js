7642d70cde81ba25c5f5eec7bb51d3511a32d0c1
K L TEJAS <91883149+IRONMAN-15@users.noreply.github.com>
2023-10-20 11:56:25 +0530

    coding test

---
+++ string.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+function readFileAsString(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
